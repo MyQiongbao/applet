b2387856b89bb7ad599aad2bb637086992b82070
unknown <jiang.mei@husor.intra>
2018-04-27 17:34:20 +0800

    base

---
+++ application/controllers/member/Welcome.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiang.mei
+ * Date: 2018/4/27
+ * Time: 17:28
+ */
+class Welcome extends CI_Controller {
+    public function index() {
+        echo 'hello, 567';
+    }
+}
